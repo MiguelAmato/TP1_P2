6a296e86f0c29c9eb77af1f4144504332281694d
Miguel Amato <miamato@ucm.es>
2021-10-23 18:56:56 +0200

    tobias puto he puesto AMATO en la linea 18

--- src/es/ucm/tp1/supercars/view/GamePrinter.java
+++ src/es/ucm/tp1/supercars/view/GamePrinter.java
@@ -7,15 +7,15 @@ public class GamePrinter {
 
 	private static final String SPACE = " ";
 
-	private static final String ROAD_BORDER_PATTERN = "═";
+	private static final String ROAD_BORDER_PATTERN = "â•�";
 
-	private static final String LANE_DELIMITER_PATTERN = "─";
+	private static final String LANE_DELIMITER_PATTERN = "â”€";
 
 	private static final int CELL_SIZE = 7;
 
 	private static final int MARGIN_SIZE = 2;
 	
-	private static final String EXIT_MSG =  "Player leaves the game";
+	private static final String EXIT_MSG =  "Player leaves the game! AMATO";
 	
 	private String indentedRoadBorder;
 
